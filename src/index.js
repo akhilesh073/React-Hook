1b255e902f5ee37d8cfa59682e914ba43f560cac
Akhil <akhilesh.073@gmail.com>
2019-12-10 11:39:05 +0530

    UseStateDataFetching
    Data Fetching using useState and useEffect

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // import Example from "./Example";
-import UseReducerHook from "./UseReducerHook";
+// import UseReducerHook from "./UseReducerHook";
+import UseStateDataFetching from "./UseStateDataFetching";
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
       <h1>React Hook examples</h1>
+      <hr />
       {/* <Example /> */}
-      <UseReducerHook />
+      {/* <UseReducerHook /> */}
+      <UseStateDataFetching />
     </div>
   );
 }
